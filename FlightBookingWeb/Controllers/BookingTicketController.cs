8a065638c50214d132afed990f16ae37a7f245b8
Bảo Nguyễn Phan Anh <102230282@sv1.dut.udn.vn>
2025-04-10 22:34:03 +0700

    why

---
+++ FlightBookingWeb/Controllers/BookingTicketController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBookingWeb.Controllers
+{
+    public class BookingTicketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Book()
+        {
+            return "Booking Ticket";
+        }
+    }
+}
